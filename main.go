9691cacff91df4436cce4f7ad93625fbaf2839c6
Matthias Luebken <matthias.luebken@gmail.com>
2022-03-06 14:50:19 +0100

    refactoring
    Signed-off-by: Matthias Luebken <matthias.luebken@gmail.com>

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/google/go-github/v42/github"
+	"github.com/luebken/awesome-crossplane-providers/query"
 	"github.com/luebken/awesome-crossplane-providers/util"
 	"golang.org/x/oauth2"
 )
@@ -33,7 +33,7 @@ func main() {
 	crdsTotalV1 := 0
 	crdsTotalTotal := 0
 
-	for _, repo := range queryRepos() {
+	for _, repo := range query.QueryPotentialProviderRepos(client, ctx) {
 		fmt.Print(".") // progress indicator
 		release, _, err := client.Repositories.GetLatestRelease(ctx, *repo.Owner.Login, *repo.Name)
 		last_release := ""
@@ -43,7 +43,7 @@ func main() {
 		crdsBeta := 0
 		crdsV1 := 0
 		if err != nil {
-			//fmt.Println("err ", err)
+			fmt.Println("err ", err)
 		} else {
 			last_release = release.CreatedAt.Time.Format("2006-01-02")
 			docs_url = "https://doc.crds.dev/github.com/" + *repo.GetOwner().Login + "/" + *repo.Name + "@" + *release.TagName
@@ -52,7 +52,7 @@ func main() {
 			crdsBeta = util.GetNumberOfCRDs(docs_url).Beta
 			crdsV1 = util.GetNumberOfCRDs(docs_url).V1
 		}
-		if !*repo.Archived {
+		if !*repo.Archived { // double check
 			desc := ""
 			subscribersCount := 0
 			if repo.Description != nil {
@@ -106,92 +106,4 @@ func main() {
 
 	result += summary
 	util.WriteToFile(result)
-}
-
-func queryRepos() (repos []*github.Repository) {
-	fmt.Println("Searching for potential Crossplane provider repos.")
-
-	// search for common phrases in readme, name, description
-	searchQueries := []string{
-		`archived:false in:readme,name,description "is a crossplane provider"`,
-		`archived:false in:readme,name,description "is a minimal crossplane provider"`,
-		`archived:false in:readme,name,description "is an experimental crossplane provider"`,
-		`archived:false in:readme,name,description "crossplane infrastructure provider"`,
-	}
-	for _, q := range searchQueries {
-		opt := &github.SearchOptions{
-			ListOptions: github.ListOptions{PerPage: 100},
-		}
-		for {
-			fmt.Print(".")
-			result, resp, err := client.Search.Repositories(ctx, q, opt)
-			if err != nil {
-				fmt.Println("err ", err)
-			}
-			repos = append(repos, result.Repositories...)
-
-			if resp.NextPage == 0 {
-				break
-			}
-			opt.Page = resp.NextPage
-		}
-	}
-
-	// crossplane orgs repos with "provider"
-	crossplaneOrgs := []string{
-		"crossplane-contrib", "crossplane",
-	}
-	for _, o := range crossplaneOrgs {
-		for {
-			opt := &github.RepositoryListOptions{
-				ListOptions: github.ListOptions{PerPage: 100},
-			}
-			fmt.Print(".")
-			newRepos, resp, err := client.Repositories.List(ctx, o, opt)
-			if err != nil {
-				fmt.Println("err ", err)
-			}
-
-			for _, r := range newRepos {
-				if strings.Contains(*r.Name, "provider") {
-					repos = append(repos, r)
-				}
-			}
-
-			if resp.NextPage == 0 {
-				break
-			}
-			opt.Page = resp.NextPage
-		}
-	}
-
-	repos = removeDuplicateRepos(repos)
-	repos = removeBlacklist(repos)
-	fmt.Printf("\nAnalyzing %d provider repos.\n", len(repos))
-
-	return repos
-}
-
-func removeBlacklist(repos []*github.Repository) []*github.Repository {
-	list := []*github.Repository{}
-	blacklist := "https://github.com/terrytangyuan/awesome-argo "
-	blacklist += " "
-	for _, repo := range repos {
-		if !strings.Contains(blacklist, *repo.HTMLURL) {
-			list = append(list, repo)
-		}
-	}
-	return list
-}
-
-func removeDuplicateRepos(repos []*github.Repository) []*github.Repository {
-	keys := make(map[string]bool)
-	list := []*github.Repository{}
-	for _, repo := range repos {
-		if _, value := keys[*repo.HTMLURL]; !value {
-			keys[*repo.HTMLURL] = true
-			list = append(list, repo)
-		}
-	}
-	return list
 }
